715a048b19e973d0eb90d1540f31b69660220d95
Himalayan <hdnpress@gmail.com>
2023-02-24 13:54:26 +0545

    Create New Category Modal and Save a Category

--- app/Http/Livewire/CategoriesList.php
+++ app/Http/Livewire/CategoriesList.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -10,10 +11,44 @@ class CategoriesList extends Component
 {
     use WithPagination;
 
+    public Category $category;
+
+    public bool $showModal = false;
+
+    public function openModal()
+    {
+        $this->showModal = true;
+
+        $this->category = new Category();
+    }
+
+    public function updatedCategoryName()
+    {
+        $this->category->slug = Str::slug($this->category->name);
+    }
+
+    protected function rules(): array
+    {
+        return [
+            'category.name' => ['required', 'string', 'min:3'],
+            'category.slug' => ['nullable', 'string'],
+        ];
+    }
+
     public function render()
     {
-        $categories = Category::paginate(10);
+        $categories = Category::latest()
+            ->paginate(10);
 
         return view('livewire.categories-list', ['categories' => $categories]);
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->category->save();
+
+        $this->reset('showModal');
     }
 }
